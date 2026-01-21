6d276003c1c387d1f746190ba0cb60064a71b2c2
kudeee <codysu08@gmail.com>
2026-01-21 21:25:21 +0800

    added payment method

--- js/sign-up-page.js
+++ js/sign-up-page.js
@@ -4,9 +4,12 @@ document.addEventListener("DOMContentLoaded", () => {
   const next = document.getElementById("next-btn");
   const prev = document.getElementById("prev-btn");
   const lastPrev = document.getElementById("last-prev-btn");
+  const secondLastPrev = document.getElementById("second-last-prev-btn");
+  const secondLastNext= document.getElementById("second-last-next-btn");
   const f_page = document.getElementById("first-page");
   const s_page = document.getElementById("second-page");
-  const t_page = document.getElementById("last-page");
+  const t_page = document.getElementById("second-last-page");
+  const l_page = document.getElementById("last-page");
 
   fnext.addEventListener("click", () => {
     f_page.style.display = "none";
@@ -23,8 +26,18 @@ document.addEventListener("DOMContentLoaded", () => {
     t_page.style.display = "block";
   });
 
-  lastPrev.addEventListener("click", () => {
+  secondLastPrev.addEventListener("click", () => {
     s_page.style.display = "block";
     t_page.style.display = "none";
+  })
+
+  secondLastNext.addEventListener("click", () => {
+    t_page.style.display = "none";
+    l_page.style.display = "block";
+  })
+
+  lastPrev.addEventListener("click", () => {
+    t_page.style.display = "block";
+    l_page.style.display = "none";
   });
 });
